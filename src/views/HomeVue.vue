abedc93699902f0959d87b96059bb729e454ce94
Yurii Rybachok <drummer2009@ukr.net>
2022-12-30 17:47:13 +0200

    using and importing carousel and slide component

---
+++ src/views/HomeVue.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <CarouselVue class="carousel">
+      <SlideVue>
+        <div>
+          <p>Hello</p>
+        </div>
+      </SlideVue>
+    </CarouselVue>
+  </div>
+</template>
+
+<script>
+import CarouselVue from '@/components/CarouselVue.vue';
+import SlideVue from '@/components/SlideVue.vue';
+
+export default {
+  name: 'HomeVue',
+  components: {
+    CarouselVue,
+    SlideVue
+  },
+  setup() {
+    
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.carousel {
+  max-height: 100vh;
+  height: 100vh;
+  
+}
+</style>
